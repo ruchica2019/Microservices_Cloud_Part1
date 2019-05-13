d8a88f7a365bfbf006758f189cd20eac3ef9dd27
ruchica2019 <46400267+ruchica2019@users.noreply.github.com>
2019-05-13 10:55:10 +0530

    First Commit

---
+++ JavaSpringCode/MicroservicesSpringCloud_Part1/src/test/java/com/microservices/cloud/MicroservicesSpringCloud_Part1/MicroservicesSpringCloudPart1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservices.cloud.MicroservicesSpringCloud_Part1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesSpringCloudPart1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
