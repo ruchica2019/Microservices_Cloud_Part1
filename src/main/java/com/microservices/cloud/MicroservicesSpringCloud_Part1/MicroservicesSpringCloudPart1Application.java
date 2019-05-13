6d2234518b64f56dc7d8274060a71944e42bc6bc
ruchica2019 <46400267+ruchica2019@users.noreply.github.com>
2019-05-13 11:06:38 +0530

    First Commit

---
+++ src/main/java/com/microservices/cloud/MicroservicesSpringCloud_Part1/MicroservicesSpringCloudPart1Application.java
@@ -0,0 +1,13 @@
+package com.microservices.cloud.MicroservicesSpringCloud_Part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringCloudPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesSpringCloudPart1Application.class, args);
+	}
+
+}
