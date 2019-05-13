ef56351697a293e49bf9522c3114bc5cf487eefc
ruchica2019 <46400267+ruchica2019@users.noreply.github.com>
2019-05-13 10:26:42 +0530

    Baseline Checkin

---
+++ SpringCloud/Microservices_Cloud_Part1/Microservices_Cloud_1/src/main/java/com/microservices/cloud/Microservices_Cloud_1/App.java
@@ -0,0 +1,13 @@
+package com.microservices.cloud.Microservices_Cloud_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
